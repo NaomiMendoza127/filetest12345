b7fafa13689e8c76cf04affbc40954921ed8774f
NaomiMendoza127 <naomimendoza127@gmail.com>
2025-06-10 15:11:43 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,8 +1,22 @@
-if exist C:\Windows\Temp\updater_log.txt (
-    echo Log file exists
-) else (
-    echo Log file does not exist
-)
+# Define a log file path accessible by most users (including LocalSystem)
+$logPath = "C:\Windows\Temp\updater_log.txt"
+
+try {
+    # Get the current user running the script
+    $currentUser = whoami
+
+    # Create a log message with date and user info
+    $logMessage = "UpdaterSrv script executed at $(Get-Date) by user $currentUser"
+
+    # Append the log message to the log file
+    $logMessage | Out-File -FilePath $logPath -Append -Encoding utf8
+}
+catch {
+    # If writing to log fails, capture error details
+    $errorMessage = "Failed to write log at $(Get-Date). Error: $_"
+    $errorMessage | Out-File -FilePath $logPath -Append -Encoding utf8
+}
+
 $ErrorActionPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
 $InformationPreference = 'SilentlyContinue'
